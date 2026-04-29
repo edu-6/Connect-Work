bc0b11cf14c6581c38359cf722364d9bd91df061
edu <edudiazdl326@gmail.com>
2026-04-29 13:55:05 -0600

    Completacion de perfil para cliente

--- connect-work-frontend/src/componentes/usuarios/perfiles/cliente-perfil-form/cliente-perfil-form.ts
+++ connect-work-frontend/src/componentes/usuarios/perfiles/cliente-perfil-form/cliente-perfil-form.ts
@@ -1,9 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ErrorBackend } from '../../../../modelos/ErrorBackend';
+import { ClientePefil } from '../../../../modelos/perfiles/usuarioPerfil';
+import { PerfilesService } from '../../../../servicios/perfiles-service';
 
 @Component({
   selector: 'app-cliente-perfil-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cliente-perfil-form.html',
   styleUrl: './cliente-perfil-form.css',
 })
-export class ClientePerfilForm {}
+export class ClientePerfilForm implements OnInit {
+  formulario!: FormGroup;
+
+  intentoEnviarlo = signal<boolean>(false);
+  hayError = signal<boolean>(false);
+  mensajeError!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private perfilesService: PerfilesService,
+    private router: Router
+  )
+   {
+    
+   }
+
+  ngOnInit(): void {
+    this.instanciarFormulario();
+  }
+
+  private instanciarFormulario() {
+    this.formulario = this.formBuilder.group({
+      cuiUsuario: [localStorage.getItem('cui')],
+      descripcion: ["", [Validators.required, Validators.maxLength(500)]],
+      sitioWeb: ["", [ Validators.minLength(1),Validators.maxLength(100)]],
+      industria: ["", [Validators.required, Validators.maxLength(100)]],
+    });
+  }
+
+  enviar() {
+    this.hayError.set(false);
+    this.intentoEnviarlo.set(true);
+    if (!this.formulario.valid) return;
+
+    let nuevo = this.formulario.value as ClientePefil;
+    this.guardarNuevo(nuevo);
+  }
+
+  private guardarNuevo(nuevo: ClientePefil) {
+    this.perfilesService.crearPefilcliente(nuevo).subscribe({
+      next: () => {
+        this.redirigirAHome();
+      },
+      error: (error: any) => {
+        this.registrarError(error);
+      }
+    });
+  }
+
+  public redirigirAHome() {
+    this.router.navigate(['']);
+  }
+
+  private registrarError(httpError: any) {
+    this.hayError.set(true);
+    const errorData: ErrorBackend = httpError.error;
+    this.mensajeError = errorData.detalles;
+  }
+}
--- connect-work-frontend/src/pages/usuarios/perfiles/completacion-perfil-cliente-page/completacion-perfil-cliente-page.ts
+++ connect-work-frontend/src/pages/usuarios/perfiles/completacion-perfil-cliente-page/completacion-perfil-cliente-page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ClientePerfilForm } from '../../../../componentes/usuarios/perfiles/cliente-perfil-form/cliente-perfil-form';
 
 @Component({
   selector: 'app-completacion-perfil-cliente-page',
-  imports: [],
+  imports: [ClientePerfilForm],
   templateUrl: './completacion-perfil-cliente-page.html',
   styleUrl: './completacion-perfil-cliente-page.css',
 })
 export class CompletacionPerfilClientePage {
-  
-  
+
+
 }
